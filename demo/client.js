5459586e5ae5147fde0612a1e6ea05bc9aa8be47
Lanfei <CooLanfei@163.com>
2015-07-28 14:58:37 +0800

    Optimization

--- demo/client.js
+++ demo/client.js
@@ -4,10 +4,13 @@ var client = ws.connect('ws://localhost:8000', function (session) {
 	session.setEncoding('utf8');
 	session.send('Client');
 	session.on('data', function (data) {
-		console.log('server:', data);
+		console.log('server msg:', data);
+	});
+	session.on('end', function (code) {
+		console.log('server end:', code);
 	});
 	session.on('close', function () {
-		console.log('close');
+		console.log('server close');
 	});
 });
 client.on('error', function (error) {
--- demo/server.js
+++ demo/server.js
@@ -1,25 +1,25 @@
 var ws = require('../lib/websocket');
 
 var server = ws.createServer(function (session) {
-	console.log('client: connect');
+	console.log('client connect');
 	session.on('data', function (data) {
-		console.log('client:', data);
+		console.log('client msg:', data);
 		this.send('Hi, ' + data);
 	});
-	session.on('end', function () {
-		console.log('client: end');
+	session.on('end', function (code) {
+		console.log('client end:', code);
 	});
 	session.on('close', function () {
-		console.log('client: close');
+		console.log('client close');
 	});
 });
 
 server.on('listening', function () {
-	console.log('server: listening');
+	console.log('server listening');
 });
 
 server.on('close', function () {
-	console.log('server: close');
+	console.log('server close');
 });
 
 server.on('error', function (error) {
